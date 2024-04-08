efdecea28c2f052e0ce42f3e674f41cb63bf8891
MHVKumara <harshanimonika977@gmail.com>
2024-04-08 22:36:34 +0530

    modify invoices.js

--- invoicing-api/routes/invoices.js
+++ invoicing-api/routes/invoices.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Invoice = require('../models/invoices');
+const Invoice = require('./models/invoice');
 
 router.post('/', async (req, res) => {
   try {
